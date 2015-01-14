6cc5be305b1e3d07fb8c4038cf49ec813bfe8afd
syonidv <syonidv@hotmail.com>
2015-01-14 00:46:05 -0500

    Initial commit.

---
+++ chrome/content/cloudgem.js
@@ -0,0 +1,8 @@
+
+window.addEventListener( "load", function() {
+	setInterval( function() {
+		console.log( "Cloudgem service started (2)." );
+	}, 2000 );
+}, false );
+
+console.log( "Cloudgem service started (1)." );
